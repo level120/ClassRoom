37c91a2124adee90a3e986104b42522b536c2de9
Gusam Park <tnara9573@gmail.com>
2016-10-15 16:59:10 +0900

    add
    add algorithm using c & cpp

---
+++ mergesort.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+ 
+#define MAX_SIZE 8
+ 
+//주석 풀면 연산 과정 보여줌
+
+void mergesort( int n, int* S );
+void merge( int h, int m, int* U, int* V, int* S );
+void copy( int* _origin, int* _new, int o_start, int n_start, int end );
+void init( int n, int* S );
+void print( int n, int* S );
+ 
+int main()
+{
+    int s[ MAX_SIZE ];
+    init( MAX_SIZE, s);
+    print( MAX_SIZE, s );
+    mergesort( MAX_SIZE, s );
+    print( MAX_SIZE, s );
+    return 0;
+}
+ 
+void mergesort( int n, int* S )
+{
+    if ( n > 1 )
+    {
+        const int h = n / 2, m = n - h;
+        int* U = ( int* )calloc( h, sizeof( int ) );
+        int* V = ( int* )calloc( m, sizeof( int ) );
+ 
+        copy( S, U, 0, 0, h );
+	//print( h, U );
+        copy( S, V, h, 0, n );
+	//print( n, V );
+ 
+        mergesort( h, U );
+        mergesort( m, V );
+ 
+        merge( h, m, U, V, S );
+ 
+        free( U );
+        free( V );
+    }
+}
+ 
+void merge( int h, int m, int* U, int* V, int* S )
+{
+    int i = 0, j = 0, k = 0;
+ 
+    while ( i < h && j < m )
+    {
+        if ( U[ i ] < V[ j ] )
+            S[ k++ ] = U[ i++ ];
+        else
+            S[ k++ ] = V[ j++ ];
+    }
+ 
+    if ( i >= h )
+        copy( V, S, j, k, m );
+    else
+        copy( U, S, i, k, h );
+    //print( MAX_SIZE, S );
+}
+ 
+void copy( int* _origin, int* _new, int o_start, int n_start, int end )
+{
+    int i;
+    for ( i = o_start; i < end; i++ )
+    {
+        _new[ n_start++ ] = _origin[ o_start++ ];
+    }
+}
+ 
+void init( int n, int* S )
+{
+    int i;
+    srand( time( NULL ) );
+ 
+    for ( i = 0; i < n; i++ )
+        S[ i ] = rand() % 100 + 1;
+}
+ 
+void print( int n, int* S )
+{
+    for ( int i = 0; i < n; i++ )
+        printf( "%d ", S[ i ] );
+    puts( "" );
+}
