b014ff93a20ea89a5b0fdd123e36d436ffaa39aa
Gusam Park <tnara9573@gmail.com>
2017-05-17 03:13:53 +0900

    Update

---
+++ UnKnown/OperatingSystem(0409)/WpfApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Algorithm name()
+            TestCase test = new TestCase();
+
+            test.check();
+
+            FCFS fcfs = new FCFS(test.data);
+
+            test.check();
+
+            Console.WriteLine("평균대기시간 : " + fcfs.avg_wait());
+            Console.WriteLine("평균반환시간 : " + fcfs.avg_return());
+            Console.WriteLine();
+        }
+    }
+}
